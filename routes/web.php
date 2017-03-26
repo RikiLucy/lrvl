d445eb85e3a42da01d9315c379d80a5b72d2760e
RikiLucy <riki.and.lucy@gmail.com>
2017-03-26 22:05:34 +0700

    маршрутизатор

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,35 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'web'], function(){
+
+    Route::match(['get', 'post'], '/', ['uses' => 'IndexController@execute', 'as' => 'home']);
+    Route::get('page/{alias}', ['uses' => 'PageController@execute', 'as' => 'page']);
+
+    Route::auth();
+});
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function(){
+
+    Route::get('/', function (){
+
+    });
+    Route::group(['prefix' => 'pages'], function (){
+        Route::get('/', ['uses' => 'PagesController@execute', 'as' => 'pages']);
+        Route::match(['get', 'post'], '/add', ['uses' => 'PagesAddController@execute', 'as' => 'pagesAdd']);
+        Route::match(['get', 'post', '/delete'], '/edit{page}', ['uses' => 'PagesEditController@edit', 'as' => 'pagesEdit']);
+    });
+
+    Route::group(['prefix' => 'portfolios'], function (){
+        Route::get('/', ['uses' => 'PortfolioController@execute', 'as' => 'portfolio']);
+        Route::match(['get', 'post'], '/add', ['uses' => 'PortfolioAddController@execute', 'as' => 'portfolioAdd']);
+        Route::match(['get', 'post', '/delete'], '/edit{portfolio}', ['uses' => 'PortfolioEditController@edit', 'as' => 'portfolioEdit']);
+    });
+
+    Route::group(['prefix' => 'services'], function (){
+        Route::get('/', ['uses' => 'ServiceController@execute', 'as' => 'services']);
+        Route::match(['get', 'post'], '/add', ['uses' => 'ServiceAddController@execute', 'as' => 'serviceAdd']);
+        Route::match(['get', 'post', '/delete'], '/edit{service}', ['uses' => 'ServiceEditController@edit', 'as' => 'serviceEdit']);
+    });
+
 });
